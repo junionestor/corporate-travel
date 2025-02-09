889a07ae588921274d1ab11426e8fc70f6ff647f
Nestor Júnio <junionestor@hotmail.com>
2025-02-09 16:41:19 -0300

    feat: cria migrations

---
+++ database/migrations/2025_02_09_185431_create_travel_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('travel_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('travel_order_id', 100);
+            $table->string('name', 100);
+            $table->string('destination');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->foreignId('order_status_id')
+                ->default(1)
+                ->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travel_orders');
+    }
+};
