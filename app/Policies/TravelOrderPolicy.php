24f18bd4acd70253a3767dbb1c3ff248b897e94c
Nestor Júnio <junionestor@hotmail.com>
2025-02-10 17:15:42 -0300

    feat: cria policy

--- app/Policies/TravelOrderPolicy.php
+++ app/Policies/TravelOrderPolicy.php
@@ -4,62 +4,27 @@ namespace App\Policies;
 
 use App\Models\TravelOrder;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class TravelOrderPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        return false;
-    }
-
-    /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, TravelOrder $travelOrder): bool
+    public function view(User $user, TravelOrder $travelOrder): Response
     {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return false;
+        return $user->id === $travelOrder->user_id
+            ? Response::allow()
+            : Response::denyAsNotFound();
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, TravelOrder $travelOrder): bool
+    public function update(User $user, TravelOrder $travelOrder): Response
     {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, TravelOrder $travelOrder): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, TravelOrder $travelOrder): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, TravelOrder $travelOrder): bool
-    {
-        return false;
+        return $user->id === $travelOrder->user_id
+            ? Response::denyAsNotFound()
+            : Response::allow();
     }
 }
