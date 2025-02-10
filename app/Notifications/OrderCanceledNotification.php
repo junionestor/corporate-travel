e45b2f39264369d3dea9ac2eec6a056fc759e9d9
Nestor Júnio <junionestor@hotmail.com>
2025-02-10 17:09:08 -0300

    feat: notificação via email de ordem aprovado e ordem cancelada

---
+++ app/Notifications/OrderCanceledNotification.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\TravelOrder;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderCanceledNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public private(set) TravelOrder $travelOrder
+    ) {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->greeting("Olá {$this->name}!")
+            ->line("O seu pedido {$this->travelOrder->ordtravel_order_ider} foi cancelado.")
+            ->line('Qualquer dúvida entre em contado com o suporte!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
